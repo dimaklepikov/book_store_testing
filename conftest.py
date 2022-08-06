774f472cf69ca6bbaf7d2470665e8d3a77fbebab
Dmitriy Klepikov <klepikovdima@gmail.com>
2022-08-06 11:19:39 +0300

    Initialized project

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Set language: [en-gb, ru, es]")
+
+                    
+def pytest_generate_tests(metafunc):
+    option_value = metafunc.config.getoption("language")
+    if 'language' in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("language", [option_value])
+
+@pytest.fixture(scope="session")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+import time
+
+def test_guest_can_go_to_login_page(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+    time.sleep(5)
