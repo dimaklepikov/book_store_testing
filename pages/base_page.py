e82a711fc0afbbd629ae7fafc732e321a1235041
Dmitriy Klepikov <klepikovdima@gmail.com>
2022-08-06 18:27:12 +0300

    Add assertation method to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException, UnexpectedTagNameException
+
 class BasePage():
 
-    def __init__(self, browser, url) -> None:
+    def __init__(self, browser, url, timeout=3) -> None:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException, NoSuchAttributeException):
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
